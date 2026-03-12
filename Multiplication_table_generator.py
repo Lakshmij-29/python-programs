39cc9af68b15ec47fdf5ffa215a357835e4bdba3
Lakshmi J <lak911068shmi@gmail.com>
2026-03-12 20:06:06 +0530

    Create Multiplication_table_generator.py

---
+++ Multiplication_table_generator.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a number: "))
+
+print("Multiplication table for", num)
+
+for i in range(1, 11):
+    result = num * i
+    print(num, "x", i, "=", result)
+
+print("Table generated successfully!")
