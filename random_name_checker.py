1d8bcec07bd36ffa72dacec15950768771e7f06d
Lakshmi J <lak911068shmi@gmail.com>
2026-03-27 22:45:42 +0530

    Create random_name_checker.py

---
+++ random_name_checker.py
@@ -0,0 +1,13 @@
+import random
+
+names = ["Alex", "John", "Sam", "Riya", "Neha"]
+
+print("Participants:", names)
+
+winner = random.choice(names)
+
+print("Selected Winner:", winner)
+
+print("Congratulations ")
+
+print("Selection completed")
