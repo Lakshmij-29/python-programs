666762df8e3be526353b70200a291898afaf1b5a
Lakshmi J <lak911068shmi@gmail.com>
2026-04-11 20:11:08 +0530

    Create Simple_Classification_(Decision_Tree).py

---
+++ Simple_Classification_(Decision_Tree).py
@@ -0,0 +1,11 @@
+from sklearn.tree import DecisionTreeClassifier
+
+X = [[1],[2],[3],[4]]
+y = [0,0,1,1]
+
+model = DecisionTreeClassifier()
+model.fit(X, y)
+
+pred = model.predict([[2.5]])
+
+print("Prediction:", pred[0])
