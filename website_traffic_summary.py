5a49cad783e0403fbd978c83ad9e161f2eecca92
Lakshmi J <lak911068shmi@gmail.com>
2026-04-30 21:10:27 +0530

    Create website_traffic_summary.py

---
+++ website_traffic_summary.py
@@ -0,0 +1,9 @@
+
+visits = [120, 150, 90, 200, 170, 300, 250]
+
+print("Total Visits:", sum(visits))
+print("Highest Day:", max(visits))
+print("Lowest Day:", min(visits))
+
+avg = sum(visits) / len(visits)
+print("Average Visits:", round(avg, 2))
