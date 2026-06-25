f1dabcbfeb9eac239a44c6e77a225b691842b976
Lakshmi J <lak911068shmi@gmail.com>
2026-06-25 20:45:08 +0530

    Create cloud_cost_estimator.py

---
+++ cloud_cost_estimator.py
@@ -0,0 +1,10 @@
+compute_cost = 2500
+storage_cost = 1200
+network_cost = 800
+
+total = compute_cost + storage_cost + network_cost
+
+print("Compute Cost:", compute_cost)
+print("Storage Cost:", storage_cost)
+print("Network Cost:", network_cost)
+print("Total Monthly Cost:", total)
