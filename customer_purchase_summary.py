7fc0260952c48717db8c96562038cb6ff9753050
Lakshmi J <lak911068shmi@gmail.com>
2026-07-03 21:49:01 +0530

    Create customer_purchase_summary.py

---
+++ customer_purchase_summary.py
@@ -0,0 +1,9 @@
+purchases = [450, 780, 1200, 560]
+
+total = sum(purchases)
+average = total / len(purchases)
+
+print("Total Purchase:", total)
+print("Average Purchase:", average)
+
+print("Highest Purchase:", max(purchases))
