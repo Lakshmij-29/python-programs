d0b7da582268eb092b5089a2f7a6cebf2ef329ff
Lakshmi J <lak911068shmi@gmail.com>
2026-07-28 23:13:54 +0530

    Create monthly_revenue_report.py

---
+++ monthly_revenue_report.py
@@ -0,0 +1,9 @@
+revenue = [120000, 135000, 128000, 142000]
+
+total = sum(revenue)
+average = total / len(revenue)
+
+print("Total Revenue:", total)
+print("Average Revenue:", average)
+print("Highest Revenue:", max(revenue))
+print("Lowest Revenue:", min(revenue))
