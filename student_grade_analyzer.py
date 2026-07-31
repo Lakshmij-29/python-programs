6e874a46c8a7239c56c5f606044c05b382e6c55b
Lakshmi J <lak911068shmi@gmail.com>
2026-07-31 23:10:57 +0530

    Create student_grade_analyzer.py

---
+++ student_grade_analyzer.py
@@ -0,0 +1,10 @@
+marks = [82, 76, 91, 88, 69]
+
+average = sum(marks) / len(marks)
+
+print("Average:", round(average, 2))
+print("Highest:", max(marks))
+print("Lowest:", min(marks))
+
+if average >= 75:
+    print("Overall Performance: Good").
