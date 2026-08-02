474d26ec7d88866866990b45482826833efc9080
Lakshmi J <lak911068shmi@gmail.com>
2026-08-02 20:50:26 +0530

    Create website_visitor_statistics.py

---
+++ website_visitor_statistics.py
@@ -0,0 +1,6 @@
+visitors = [320, 410, 380, 450, 520]
+
+print("Total Visitors:", sum(visitors))
+print("Average Visitors:", sum(visitors) / len(visitors))
+print("Peak Visitors:", max(visitors))
+print("Lowest Visitors:", min(visitors))
