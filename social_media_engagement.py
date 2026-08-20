c361b963e011ce58cf8b7495d2b2427424ce8a8f
Lakshmi J <lak911068shmi@gmail.com>
2026-08-21 00:04:22 +0530

    Create social_media_engagement.py

---
+++ social_media_engagement.py
@@ -0,0 +1,11 @@
+likes = 4200
+comments = 380
+shares = 620
+followers = 25000
+
+engagement = ((likes + comments + shares) / followers) * 100
+
+print("Likes:", likes)
+print("Comments:", comments)
+print("Shares:", shares)
+print("Engagement Rate:", round(engagement, 2), "%")
